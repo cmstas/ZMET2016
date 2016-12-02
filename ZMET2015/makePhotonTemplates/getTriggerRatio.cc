a64c07a3435fad4af27d769c957c72dc4e0ac4dd
cwelke <cwelke@cern.ch>
2016-12-02 05:05:21 -0800

    update dataset for prescale derivation

--- ZMET2015/makePhotonTemplates/getTriggerRatio.cc
+++ ZMET2015/makePhotonTemplates/getTriggerRatio.cc
@@ -1,4 +1,5 @@
 #include "TChain.h"
+#include "TCut.h"
 #include <iostream>
 
 using namespace std;
@@ -8,20 +9,22 @@ void getTriggerRatio()
 
   TChain ch("t");
 
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016B_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016C_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016D_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016E_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016F_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016G_23sep2016rereco_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016H_Prompt_ph_v*.root"));
+	ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-22-05/data_2016H_Prompt_ph_v*.root"));
 
-	// ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-11-00/data_2016B_Prompt_ph_v2*.root"));
-	// ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-11-00/data_2016C_Prompt_ph_v2*.root"));
-	// ch.Add(Form("/nfs-7/userdata/ZMEToutput/output/ZMETbabies/V08-11-00/data_2016D_Prompt_ph_v2*.root"));
-
-	ch.Add(Form("/hadoop/cms/store/user/cwelke/uaf8/dilepbabies/V08-11-01_data_2016B_Prompt_ph_v2/*.root"));
-	ch.Add(Form("/hadoop/cms/store/user/cwelke/uaf8/dilepbabies/V08-11-01_data_2016C_Prompt_ph_v2/*.root"));
-	ch.Add(Form("/hadoop/cms/store/user/cwelke/uaf8/dilepbabies/V08-11-01_data_2016D_Prompt_ph_v2/*.root"));
+	TCut base("evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon165_R9Id90_HE10_IsoM>0&&gamma_pt[0]>200");
 	
-    int trig_165 = ch.GetEntries("evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon165_R9Id90_HE10_IsoM>0&&gamma_pt[0]>200");
-    int trig_22  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon22_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon22_R9Id90_HE10_IsoM");
-    int trig_30  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon30_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon30_R9Id90_HE10_IsoM");
-    int trig_36  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon36_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon36_R9Id90_HE10_IsoM");
-    int trig_50  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon50_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon50_R9Id90_HE10_IsoM");
+    int trig_165 = ch.GetEntries(base);
+    int trig_22  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon22_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon22_R9Id90_HE10_IsoM"+base);
+    int trig_30  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon30_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon30_R9Id90_HE10_IsoM"+base);
+    int trig_36  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon36_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon36_R9Id90_HE10_IsoM"+base);
+    int trig_50  = ch.GetEntries("(evt_type==2&&evt_passgoodrunlist>0&&HLT_Photon50_R9Id90_HE10_IsoM>0&& gamma_pt[0]>200)*HLT_Photon50_R9Id90_HE10_IsoM"+base);
 
 	cout<<"trig165: "<<trig_165<<endl;
 	cout<<"trig22:  "<<trig_22<<endl;
