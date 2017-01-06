2211c157862b0c588b282cc23ef1b84b4aec3b14
cwelke <cwelke@cern.ch>
2017-01-06 11:36:33 -0800

    add jec files for moriond

---
+++ dilepbabymaker/jetCorrections/source_80X/DATA/downloadJECs.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016BCDV2_DATA/Spring16_23Sep2016BCDV2_DATA_L1FastJet_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016BCDV2_DATA/Spring16_23Sep2016BCDV2_DATA_L2L3Residual_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016BCDV2_DATA/Spring16_23Sep2016BCDV2_DATA_L2Relative_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016BCDV2_DATA/Spring16_23Sep2016BCDV2_DATA_L3Absolute_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016BCDV2_DATA/Spring16_23Sep2016BCDV2_DATA_Uncertainty_AK4PFchs.txt
+
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016EFV2_DATA/Spring16_23Sep2016EFV2_DATA_L1FastJet_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016EFV2_DATA/Spring16_23Sep2016EFV2_DATA_L2L3Residual_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016EFV2_DATA/Spring16_23Sep2016EFV2_DATA_L2Relative_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016EFV2_DATA/Spring16_23Sep2016EFV2_DATA_L3Absolute_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016EFV2_DATA/Spring16_23Sep2016EFV2_DATA_Uncertainty_AK4PFchs.txt
+
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016GV2_DATA/Spring16_23Sep2016GV2_DATA_L1FastJet_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016GV2_DATA/Spring16_23Sep2016GV2_DATA_L2L3Residual_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016GV2_DATA/Spring16_23Sep2016GV2_DATA_L2Relative_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016GV2_DATA/Spring16_23Sep2016GV2_DATA_L3Absolute_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016GV2_DATA/Spring16_23Sep2016GV2_DATA_Uncertainty_AK4PFchs.txt
+
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016HV2_DATA/Spring16_23Sep2016HV2_DATA_L1FastJet_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016HV2_DATA/Spring16_23Sep2016HV2_DATA_L2L3Residual_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016HV2_DATA/Spring16_23Sep2016HV2_DATA_L2Relative_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016HV2_DATA/Spring16_23Sep2016HV2_DATA_L3Absolute_AK4PFchs.txt
+wget https://github.com/cms-jet/JECDatabase/raw/master/textFiles/Spring16_23Sep2016HV2_DATA/Spring16_23Sep2016HV2_DATA_Uncertainty_AK4PFchs.txt
