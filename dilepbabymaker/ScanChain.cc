b501304afea40f1aa674d0918788650ffa312132
olivito <dominick.olivito@gmail.com>
2017-01-12 15:16:45 -0800

    specify jecs for Summer16 MC

--- dilepbabymaker/ScanChain.cc
+++ dilepbabymaker/ScanChain.cc
@@ -264,8 +264,8 @@ void babyMaker::ScanChain(TChain* chain, std::string baby_name, int max_events){
 		}		
 	  }
 
-	  else if( TString(currentFile->GetTitle()).Contains("80MiniAODv") || TString(currentFile->GetTitle()).Contains("RelVal") ){
-		// files for 80X MC
+	  else if( TString(currentFile->GetTitle()).Contains("80MiniAODv") || TString(currentFile->GetTitle()).Contains("RelVal") || TString(currentFile->GetTitle()).Contains("Summer16") ){
+		// files for 80X MC - using also for Summer16 (Moriond17) production for now
 		jetcorr_filenames_pfL1FastJetL2L3.clear();
 		jetcorr_filenames_pfL1FastJetL2L3.push_back  ("jetCorrections/source_80X/MC/Spring16_25nsV1_MC_L1FastJet_AK4PFchs.txt"   );
 		jetcorr_filenames_pfL1FastJetL2L3.push_back  ("jetCorrections/source_80X/MC/Spring16_25nsV1_MC_L2Relative_AK4PFchs.txt"  );
