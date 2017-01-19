0b4fda017d57b4d9f2888d724fd5975746da3222
olivito <dominick.olivito@gmail.com>
2017-01-19 08:31:22 -0800

    update job inputs

--- dilepbabymaker/batchsubmit/make_job_inputs.sh
+++ dilepbabymaker/batchsubmit/make_job_inputs.sh
@@ -2,6 +2,6 @@
 
 THISDIR=`pwd`
 cd ..
-tar -czf batchsubmit/job_input/package.tar.gz *.so LinkDef*.pcm jetCorrections jsons btagsf lepsf data processBaby
+tar -czf batchsubmit/job_input/package.tar.gz *.so pileup_jul21_nominalUpDown.root xsec_higgsino.root btagsf leptonSFs TChiWZ_entries_V08-00-05_FS.root xsec_susy_13tev.root T5ZZ_entries.root jetCorrections processBaby MVAinput Cert*.txt TChiHZ_HToBB_ZToLL.root 
 cd $THISDIR
 cp wrapper_auto.sh sweeproot.sh sweeproot_macro.C merge_script.sh merge_macro.C job_input/
