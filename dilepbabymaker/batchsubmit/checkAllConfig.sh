17571764389bc334e14ac9cedc74a1ea830463ba
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-01-23 18:30:34 -0800

    Replaced old batchsubmit with Metis batchsubmit

--- dilepbabymaker/batchsubmit/checkAllConfig.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-condor_dir=$1
-
-for condorfile in $condor_dir/*; do
-    echo "RUNNING " $condorfile
-    ./resubmitConfig.sh $condorfile
-done
--- dilepbabymaker/batchsubmit/ducks.py
+++ dilepbabymaker/batchsubmit/ducks.py
@@ -1,58 +1,53 @@
-import sys
-#sys.path.insert(0,"/home/users/olivito/zmet_dev/ZMET2016/NtupleTools/AutoTwopler")
-sys.path.insert(0,"/home/users/bhashemi/Projects/GIT/ZMET2016/NtupleTools/AutoTwopler")
-import run
-import params as p
-import zmet as zmet
+import sys, os
+import time
+import itertools
+import numpy
+import ZMET_datasets as ZMET
 
-# set tag
-zmet.tag = "V08-22-18"
+from metis.Sample import DirectorySample,SNTSample
+from metis.CondorTask import CondorTask
+from metis.StatsParser import StatsParser
 
-# make instructions
-instructions = []
+exec_path = "condor_exe.sh"
+tar_path = "package.tar.gz"
+hadoop_path = "ZMET_babies"
+job_tag = "ZMET_babies"
 
-##
-## make instructions by class of sample
-##
-#samples_types = ["backgrounds", "data", "scans"]
-#samples_types = ["backgrounds", "data"]
-samples_types = ["data"]
-for stype in samples_types:
-    for ds in zmet.d_ds2name[stype].keys():
-        instructions.append({"executable": zmet.executable, "package": zmet.package, "analysis": "ZMET", "dataset": ds, "baby_tag": zmet.tag, "type": "BABY", "extra": [-1, "output.root,skim.root"]})
+datasetsToProcess = ZMET.get(year = 2017)
 
-##
-## make instructions by sample shortname
-##
-#todo = "wjets_ht1200_mgmlm_nonext ".strip().split()
-#instructions = [inst for inst in instructions if zmet.dataset_to_shortname(inst["dataset"]) in todo]
+total_summary = {}
+while True:
+    all_jobs_done = True
+    for dsname in datasetsToProcess:
+        task = CondorTask(
+              sample = SNTSample(dataset = dsname),
+              open_dataset = False,
+              flush = True,
+              files_per_output = 1,
+              output_name = "baby.root",
+              tag = job_tag,
+              cmssw_version = "CMSSW_9_4_9", # doesn't do anything
+              executable = exec_path,
+              tarfile = tar_path,
+              condor_submit_params = {"sites" : "T2_US_UCSD"},
+              special_dir = hadoop_path
+              )
+        task.process()
+        if not task.complete():
+            print("Job %s still running." % job_tag)
+            all_jobs_done = False
+        total_summary[dsname] = task.get_task_summary()
+        if task.complete():
+            print ""
+            print "Job={} finished".format(job_tag)
+            print ""
 
-#print instructions
-#sys.exit()
-
-p.dataset_to_shortname = zmet.dataset_to_shortname
-p.dashboard_name = zmet.dashboard_name
-p.sweepRoot_scripts = zmet.sweepRoot_scripts
-p.merging_scripts = zmet.merging_scripts
-p.baby_merged_dir = zmet.baby_merged_dir
-p.merge_babies_on_condor = zmet.merge_babies_on_condor
-p.exit_when_done = True
-
-run.main(instructions=instructions, params=p)
-
-# instructions = [ 
-#         {
-#             "dataset": "/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM",
-#             "type": "BABY",
-#             "extra": "",
-#             "baby_tag": "v0.01",
-#             "analysis": "ZMET",
-#             "package": ss.package,
-#             "executable": ss.executable,
-#             }
-#         ]
-
-# p.merging_scripts = ["frank/merge.sh", "frank/merge.C"]
-# p.dashboard_name = "AutoTwopler_test"
-# p.merge_babies_on_condor = True
-# run.main(instructions=instructions, params=p, do_one_iteration=True)
+    if all_jobs_done:
+        print ""
+        print "All jobs are finished."
+        print ""
+        break
+    StatsParser(data=total_summary, webdir="~/public_html/dump/ZMet_babymaking/").do()
+    os.system("chmod -R 755 ~/public_html/dump/ZMet_babymaking/")
+    print "Sleeping 300 seconds ..."
+    time.sleep(300)
