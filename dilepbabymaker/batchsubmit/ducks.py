86552798ae3bff05cce1030cf3f23de7adc22834
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-04-11 19:05:11 +0200

    Added powheg and amcnlo samples

--- dilepbabymaker/batchsubmit/ducks.py
+++ dilepbabymaker/batchsubmit/ducks.py
@@ -16,7 +16,7 @@ job_tag = "ZMET_babies"
 #datasetsToProcess = ZMET.get(data = "SinglePhoton",year = 2017)
 #EWKMCStrings = ["WGammaWJets","ttbar","SingleTop"]
 MuonDataString = "2017SingleMuon"
-datasetsToProcess = ZMET.get(data = "ttbarGamma",year = 2017)
+datasetsToProcess = ZMET.get(data = ["DYamcnlo","TTBarPowheg"],year = 2017)
 
 print("Processing these datasets")
 print(datasetsToProcess)
