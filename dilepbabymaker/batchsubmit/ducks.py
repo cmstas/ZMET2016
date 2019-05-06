427b4a22f3da4ccaf5f642a1b5bd2d9de4f65626
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-05-06 12:48:54 +0200

    New files

--- dilepbabymaker/batchsubmit/ducks.py
+++ dilepbabymaker/batchsubmit/ducks.py
@@ -18,7 +18,8 @@ job_tag = "ZMET_babies"
 MuonDataString = "2017SingleMuon"
 #datasetsToProcess = ZMET.get(data = ["DoubleMuon","DoubleEG","MuonEG","ttZ","ttW","WW","WZ","ZZ","TTBarPowheg","DYamcnlo","SingleTop"],year = 2017)
 
-datasetsToProcess = ZMET.get(data = ["GammaJetsCuttingEdge"], year = 2017)
+#datasetsToProcess = ZMET.get(data = ["GammaJetsCuttingEdge"], year = 2017)
+datsetsToProcess = ["/GJets_HT-40To100_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14_ext1-v2/MINIAODSIM"]
 
 print("Processing these datasets")
 print(datasetsToProcess)
