c92c3fcbf435b340e2f84e8e4575e9e014be4454
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-06-21 15:42:31 +0200

    Added provision to pull latest scale1fbs.txt before packaging

--- dilepbabymaker/batchsubmit/make_tarfile.sh
+++ dilepbabymaker/batchsubmit/make_tarfile.sh
@@ -1,1 +1,8 @@
+# get the latest scale1fbs.txt from CORE
+echo "Getting latest scale1fbs.txt..."
+echo "curl https://raw.githubusercontent.com/cmstas/CORE/master/Tools/datasetinfo/scale1fbs.txt > ../scale1fbs.txt"
+
+curl https://raw.githubusercontent.com/cmstas/CORE/master/Tools/datasetinfo/scale1fbs.txt > ../scale1fbs.txt
+
+#make the tarfile
 tar -zcf package.tar.gz --exclude=*.tar.gz ../
