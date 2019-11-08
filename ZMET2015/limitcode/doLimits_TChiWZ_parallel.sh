89768714f0b8cfc994e8355277ae65ea513fa182
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2019-11-08 15:37:45 -0800

    new code

--- ZMET2015/limitcode/doLimits_TChiWZ_parallel.sh
+++ ZMET2015/limitcode/doLimits_TChiWZ_parallel.sh
@@ -4,17 +4,16 @@ function process_datacard ()
 {
 
 	datacardname=`basename $1`
-	MASS1=$(echo "$datacardname" | awk '{split($0,a,"_"); print a[3]}')
-	MASS2=$(echo "$datacardname" | awk '{split($0,a,"_"); print a[5]}')
+	MASS1=$(echo "$datacardname" | awk '{split($0,a,"_"); print a[4]}')
+	MASS2=$(echo "$datacardname" | awk '{split($0,a,"_"); print a[6]}')
 
 	if [ $MASS1 -ge $MINMASS ]; then
 
-		# if [ $MASS2 -gt $(($MASS1-150)) ]; then
-		if [ -e ${INDIR}/datacard_mGluino_${MASS1}_mLSP_${MASS2}_.txt ]; then
-			echo "Running command: nice -n 10 combine -M Asymptotic -n mGluino${MASS1}_mLSP${MASS2}_ ${INDIR}/datacard_mGluino_${MASS1}_mLSP_${MASS2}_.txt > log/limit_mGluino${MASS1}_mLSP${MASS2}.txt 2>&1"
-			nice -n 10 combine -M Asymptotic -n mGluino${MASS1}_mLSP${MASS2}_ ${INDIR}/datacard_mGluino_${MASS1}_mLSP_${MASS2}_.txt  > log/limit_mGluino${MASS1}_mLSP${MASS2}.txt 2>&1
+		if [ -e ${INDIR}/datacard_all_mGluino_${MASS1}_mLSP_${MASS2}_.txt ]; then
+			echo "Running command: nice -n 10 combine -M Asymptotic -n mGluino${MASS1}_mLSP${MASS2}_ ${INDIR}/datacard_all_mGluino_${MASS1}_mLSP_${MASS2}_.txt > log/limit_mGluino${MASS1}_mLSP${MASS2}.txt 2>&1"
+			nice -n 10 combine -M Asymptotic -n mGluino${MASS1}_mLSP${MASS2}_ ${INDIR}/datacard_all_mGluino_${MASS1}_mLSP_${MASS2}_.txt  > log/limit_mGluino${MASS1}_mLSP${MASS2}.txt 2>&1
 		fi
-		
+
 		if [ -e higgsCombinemGluino${MASS1}_mLSP${MASS2}_.Asymptotic.mH120.root ]; then
 			mv higgsCombinemGluino${MASS1}_mLSP${MASS2}_.Asymptotic.mH120.root "limit_${MODEL}_${MASS1}_${MASS2}.root"
 			echo "limit_${MODEL}_${MASS1}_${MASS2}.root" >> list_$MODEL.txt
@@ -23,29 +22,23 @@ function process_datacard ()
 
 }
 
-#MODEL=T5ZZ
-#INDIR=/home/users/bhashemi/Projects/GIT/ZMETBabyLooper2017/SMSScans/DataCards/
-#INDIR=/home/users/bhashemi/Projects/GIT/ZMETBabyLooper2017/SMSScans/DataCards/
-#INDIR=/home/users/olivito/zmet_dev/ZMET2016/ZMET2015/limitcode/datacards_T5ZZ_070317/
-#MINMASS=1000
 
 MODEL=TChiWZ
-#INDIR=~/analysis/CMSSW_8_0_11/V08-11-00/ZMET2015/interpretations_ewk/datacards/
-INDIR=/home/users/olivito/zmet_dev/ZMET2016/ZMET2015/limitcode/datacards_TChiWZ_070317/
+INDIR=/home/users/bsathian/ZMet/ZMETBabyLooper2017/SMSScans/DataCards/TChiWZ/
 MINMASS=100
 
 OUTDIR=limits_${MODEL}_070317_paralleltest
 OWD=`pwd`
 
-declare -a cards=(`ls ${INDIR}/datacard_mG*.txt`)
-
-# #need to combine cards from multiple signal regions if necessary
-# for i in "${cards[@]}"
-# do
-#   if [ ! -e "$INDIR/datacard_all_$i.txt" ]; then
-#     python combineCards.py "$INDIR/datacard_"*"_$i.txt" > "$INDIR/datacard_all_$i.txt"  
-#   fi
-# done
+declare -a cards=(`ls ${INDIR}/datacard_*mG*.txt`)
+ #need to combine cards from multiple signal regions if necessary
+ for i in "${cards[@]}"
+ do
+   mG=$(echo "$i" | awk '{split($0,a,"_"); print a[3]"_"a[4]"_"a[5]"_"a[6]"_"}')
+   if [ ! -e "$INDIR/datacard_all_$mG.txt" ]; then
+     combineCards.py "$INDIR/datacard_"*"_$mG.txt" > "$INDIR/datacard_all_$mG.txt"
+   fi
+ done
 
 if [ ! -d "$OUTDIR" ]; then
   mkdir -p "$OUTDIR"
@@ -61,8 +54,9 @@ pushd $OUTDIR
 rm -f list_$MODEL.txt
 
 counter=0
+declare -a combinedCards=(`ls ${INDIR}/datacard_all_mG*.txt`)
 PID_LIST=''
-for i in "${cards[@]}"
+for i in "${combinedCards[@]}"
 do
     process_datacard $i & pid=$!
     PID_LIST+=" $pid";
